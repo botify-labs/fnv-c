0d9e390680a9c6364831fcc2531c79e094be3846
Fabien MARTY <fabien.marty@botify.com>
2023-05-31 14:35:51 +0200

    refactor tests and test fnv1/fnv1a

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import os
 
@@ -13,11 +14,35 @@ with open(JSON_FILE) as f:
 
 def test_fnv0_32():
     for d in data:
-        dta = d["data"].encode("utf8")
+        dta = base64.b64decode(d["data"].encode("utf8"))
         assert fnv_c.fnv0_32(dta) == d["fnv0_32"]
 
 
 def test_fnv0_64():
     for d in data:
-        dta = d["data"].encode("utf8")
+        dta = base64.b64decode(d["data"].encode("utf8"))
         assert fnv_c.fnv0_64(dta) == d["fnv0_64"]
+
+
+def test_fnv1_32():
+    for d in data:
+        dta = base64.b64decode(d["data"].encode("utf8"))
+        assert fnv_c.fnv1_32(dta) == d["fnv1_32"]
+
+
+def test_fnv1_64():
+    for d in data:
+        dta = base64.b64decode(d["data"].encode("utf8"))
+        assert fnv_c.fnv1_64(dta) == d["fnv1_64"]
+
+
+def test_fnv1a_32():
+    for d in data:
+        dta = base64.b64decode(d["data"].encode("utf8"))
+        assert fnv_c.fnv1a_32(dta) == d["fnv1a_32"]
+
+
+def test_fnv1a_64():
+    for d in data:
+        dta = base64.b64decode(d["data"].encode("utf8"))
+        assert fnv_c.fnv1a_64(dta) == d["fnv1a_64"]
